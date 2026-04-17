db415e0b7db4a48538e3592608a4b7a37ab57391
Maria Carter <maria@driver-digital.com>
2026-04-17 16:34:42 +0200

    Revert vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import shopify from 'vite-plugin-shopify'
-import shopifyClean from '@driver-digital/vite-plugin-shopify-clean'
+// import shopifyClean from '@driver-digital/vite-plugin-shopify-clean'
 
 export default defineConfig({
   build: {
     emptyOutDir: false, // Important: let the plugin handle cleanup, not Vite
   },
   plugins: [
-    shopifyClean(),
+    // shopifyClean(),
     shopify({
       themeRoot: '.',
       sourceCodeDir: 'frontend',
